a76b1dc4dfa4beb0ce16e07315bdf0855fe1eab7
Quentin Lem <quentinlemcode@gmail.com>
2022-06-08 20:12:44 +0200

    remove useless test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'party-anniversary'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('party-anniversary');
-  });
-
 });
