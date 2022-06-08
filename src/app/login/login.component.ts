788d2ef1431254eec316b8d2a5fad6c5579a161e
Quentin Lem <quentinlemcode@gmail.com>
2022-06-08 19:34:58 +0200

    Updated login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  form = new FormGroup({
+    nameControl: new FormControl<string>(''),
+  });
 
-  nameControl = new FormControl<string>('');
+  get nameControl() {
+    return this.form.get('nameControl')!;
+  }
 
-  constructor(private user: UserService) { }
+  constructor(private user: UserService, private router: Router) {}
 
-  submit() {
+  login() {
     if (!this.nameControl.value) return;
     this.user.register(this.nameControl.value).subscribe({
       next: () => {
-        console.log('created')
-      }
-    })
+        this.router.navigate(['/']);
+      },
+    });
   }
-
 }
