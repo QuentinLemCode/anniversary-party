7747bfa23a3020a6f04cb20ecf9290f03546a594
Quentin Lem <quentinlemcode@gmail.com>
2022-06-17 00:11:40 +0200

    fix test

--- src/app/admin/spotify-device/spotify-device.component.spec.ts
+++ src/app/admin/spotify-device/spotify-device.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SpotifyDeviceComponent } from './spotify-device.component';
 
 describe('SpotifyDeviceComponent', () => {
@@ -9,7 +10,7 @@ describe('SpotifyDeviceComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SpotifyDeviceComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SpotifyDeviceComponent);
