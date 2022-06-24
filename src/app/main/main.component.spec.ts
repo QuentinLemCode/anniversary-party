220f64200f0906fe10641270db7654e1b3ba1f30
Quentin Lemeasle <quentinlemcode@gmail.com>
2022-06-25 00:25:50 +0200

    fix tests

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { QueueService } from '../services/queue.service';
 
 import { MainComponent } from './main.component';
 
@@ -9,6 +10,14 @@ describe('MainComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MainComponent],
+      providers: [
+        {
+          provide: QueueService,
+          useValue: {
+            get: () => Promise.resolve([{ music: { id: '1' } }]),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
