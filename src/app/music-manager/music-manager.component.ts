02399537ed757ef54df1feaf5263aaa48e638ae9
Quentin Lem <quentinlemcode@gmail.com>
2022-07-02 20:08:54 +0200

    Add comment

--- src/app/music-manager/music-manager.component.ts
+++ src/app/music-manager/music-manager.component.ts
@@ -37,6 +37,12 @@ export class MusicManagerComponent
     super();
   }
 
+  // TODO implement music voting
+  // TODO implement cut music
+  // TODO show the global playlist
+  // TODO limit to 5 (or variable) music per user
+  // TODO implement music backlog
+
   ngOnInit(): void {
     this.search.valueChanges
       .pipe(
