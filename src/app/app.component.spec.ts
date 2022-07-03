2ad8f677a0a54cedb3dcacaca746cc34972b94bf
Quentin Lem <quentinlemcode@gmail.com>
2022-07-03 19:47:07 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { QueueComponent } from './components/queue/queue.component';
+import { SearchComponent } from './components/search/search.component';
 import { UserService } from './services/user.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
-      declarations: [AppComponent, QueueComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FontAwesomeModule,
+      ],
+      declarations: [AppComponent, QueueComponent, SearchComponent],
       providers: [
         {
           provide: UserService,
--- src/app/components/music/music.component.spec.ts
+++ src/app/components/music/music.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EMPTY } from 'rxjs';
+import { QueueService } from '../../services/queue.service';
+import { UserService } from '../../services/user.service';
 
 import { MusicComponent } from './music.component';
 
@@ -9,6 +13,22 @@ describe('MusicComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MusicComponent],
+      imports: [FontAwesomeModule],
+      providers: [
+        {
+          provide: QueueService,
+          useValue: {
+            forward: EMPTY,
+            delete: EMPTY,
+          },
+        },
+        {
+          provide: UserService,
+          useValue: {
+            isAdmin: false,
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MusicComponent);
