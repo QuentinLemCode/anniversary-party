3d49ced06737adc05f5db8d5b083c0e5a0b73e7f
Quentin Lem <quentinlemcode@gmail.com>
2022-07-09 03:41:11 +0200

    fix test

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { SearchComponent } from './search.component';
@@ -17,6 +18,7 @@ describe('SearchComponent', () => {
         ReactiveFormsModule,
         FormsModule,
         FontAwesomeModule,
+        RouterTestingModule,
       ],
     }).compileComponents();
 
