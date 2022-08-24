bde2796c8ff997c4d28234aa81a7d83cca45ccf4
Quentin Lemeasle <quentinlemcode@gmail.com>
2022-08-24 23:07:23 +0200

    generate qr code and adress site from current url

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  ViewChild,
+} from '@angular/core';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements AfterViewInit {
+  @ViewChild('qrcode')
+  qrcode!: ElementRef<HTMLCanvasElement>;
+
+  serverUrl: string;
+
+  constructor(@Inject(DOCUMENT) document: Document) {
+    this.serverUrl = document.location.host;
+  }
+
+  ngAfterViewInit(): void {
+    QRCode.toCanvas(this.qrcode.nativeElement, 'https://' + this.serverUrl, {
+      errorCorrectionLevel: 'H',
+      scale: 12,
+    });
+  }
+}
